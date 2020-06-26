984691e9f46ca6dfd215f8bf945fefc4e351a89f
Popsana Barida <popsyjunior@gmail.com>
2020-06-26 01:32:07 +0100

    update dependency

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('cj_codes_slave_route_limiter');
+        $treeBuilder = new TreeBuilder('cj_codes_slave_route_limiter');
+        $rootNode = $treeBuilder->getRootNode();
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
